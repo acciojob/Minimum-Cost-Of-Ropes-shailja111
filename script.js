9dc2283e4e33413e4fbe11ba09f7b96373f41852
shailja111 <122468623+shailja111@users.noreply.github.com>
2023-02-21 22:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function calculateMinCost() {
   //your code here
-  
+  const inputElement = document.getElementTagName("input")[0];
+	const inputs = inputElement.value;
+	const arr = inputs.split(',');
+
+	console.log(arr);
   
   
 }  
